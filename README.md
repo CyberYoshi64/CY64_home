71e013c5dcdea272e7403c7c2868b6631b746646
CyberYoshi64 <04jasonhecht@gmail.com>
2020-04-10 17:05:07 +0200

    Updated files through upload

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-CY64_home
+# CY64_home (GitHub website)
 
-This repository contains the code for my website
+This repository contains the files for my GitHub website.
+
+See cyberyoshi64.github.io/CY64_home/.
---
+++ style.js
@@ -0,0 +1,41 @@
+var scrolled=false;
+
+function scup() {
+  if (topy.style.bottom == "48px"){scrolled=true; topty.innerHTML='<table width=100% height=80px><tr><td style="font-size: 12px;"><center>Returning...</center></td></tr></table>'; scrollTo(0,0);}
+  scrolled=false;
+}
+var topy=document.getElementById("totop")
+var topty=document.getElementById("toptool")
+
+function scr() {
+  console.log("scrolled. y="+scrollY);
+  if (window.scrollY < 300) {
+    scrup0();
+    hdtoptoolt();
+  }else{
+    scrup1();
+  }
+}
+
+function scrup0() {
+  topy.style.bottom = "32px";
+  topy.style.opacity = "0";
+}
+
+function scrup1() {
+  topy.style.bottom = "48px";
+  topy.style.opacity = "1";
+}
+
+function shtoptoolt() {
+  if (topy.style.bottom == "48px"){
+    topty.style.bottom="120px";
+    topty.style.opacity="1";
+    topty.innerHTML='<table width=100% height=80px><tr><td style="font-size: 12px;"><center>Click this button to the top of the page.</center></td></tr></table>'
+  }
+}
+
+function hdtoptoolt() {
+  topty.style.bottom="136px";
+  topty.style.opacity="0";
+}
