9db57f8191c071c33d42b6e8004d2464292122a8
CyberYoshi64 <cyb3ryoshi64@gmail.com>
2020-05-31 11:35:08 +0200

    Updated header and 3DS changelog

--- en/style.js
+++ en/style.js
@@ -1,21 +1,18 @@
 var scrolled=false;
 
 function scup() {
-  if (topy.style.bottom == "48px"){scrolled=true; topty.innerHTML='<table width=100% height=80px><tr><td style="font-size: 12px;"><center>Returning...</center></td></tr></table>'; scrollTo(0,0);}
+  if (topy.style.bottom == "48px"){scrolled=true; scrollTo(0,0);}
   scrolled=false;
 }
 var topy=document.getElementById("totop")
-var topty=document.getElementById("toptool")
 
 function scr() {
   console.log("scrolled. y="+scrollY);
   if (window.scrollY < 720) {
     scrup0();
-    hdtoptoolt();
   }else{
     scrup1();
   }
-  topty.style.bottom="112px"
 }
 
 function scrup0() {
@@ -26,19 +23,4 @@ function scrup0() {
 function scrup1() {
   topy.style.bottom = "48px";
   topy.style.opacity = "1";
-}
-
-function shtoptoolt() {
-  if (topy.style.bottom == "48px"){
-    topty.style.right="16px"
-    topty.style.opacity="1";
-    topty.style.zIndex="1"
-    topty.innerHTML='<table width=100% height=80px><tr><td style="font-size: 12px;"><center>Click this button to the top of the page.</center></td></tr></table>'
-  }
-}
-
-function hdtoptoolt() {
-  topty.style.right="-320px"
-  topty.style.opacity="0";
-  topty.style.zIndex="30"
 }
