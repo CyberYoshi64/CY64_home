3ff56f036c4f6378d6acdf01b7d5bd582a50ca07
CyberYoshi64 <cyb3ryoshi64@gmail.com>
2020-06-25 17:42:22 +0200

    Updated changelog and improved JS code

--- assets/skkbaui/data.js
+++ assets/skkbaui/data.js
@@ -1,7 +1,9 @@
 var imgcontainer = document.getElementById("imgcnt")
+var imgcontlabel = document.getElementById("imgcnttxt")
 
-var scrlist=new Array(6)
-var scrlists=new Array(6)
+var scrlist=new Array(7)
+var scrlists=new Array(7)
+var debug=false
 
 scrlist[0]="scr0000.png"; scrlists[0]="n3ds_logo.png"
 scrlist[1]="scr0001.png"; scrlists[1]="n3ds_logo.png"
@@ -9,6 +11,7 @@ scrlist[2]="scr0002.png"; scrlists[2]="n3ds_logo.png"
 scrlist[3]="scr0003.png"; scrlists[3]="n3ds_logo.png"
 scrlist[4]="scr0004.png"; scrlists[4]="n3ds_logo.png"
 scrlist[5]="scr0005.png"; scrlists[5]="n3ds_logo.png"
+scrlist[6]="scr0006.png"; scrlists[6]="switch_logo.png"
 var iterator = 0
 
 function doOnce(){
@@ -35,6 +38,12 @@ function doOnce(){
 }
 doOnce()
 var interval = window.setInterval(function () {
-  imgcontainer.scrollTo(0,240 * iterator)
   iterator = (iterator + 1) % scrlist.length
+  imgcontainer.scrollTo(0,240 * iterator)
 }, 5000);
+
+if (debug == true) {
+  var interval2 = window.setInterval(function () {
+    imgcontlabel.innerHTML = "Debug info:<br><p>Scroll register: "+imgcontainer.scrollTop+"<br>Iterator: "+iterator+"<p>Currently showing:<br>"+scrlist[iterator]+"; "+scrlists[iterator]+"</p></p>"
+  }, 34);
+}
